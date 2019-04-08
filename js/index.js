719017c56de843a25451e39cfacf8b3d3e4a610a
Patryk Abraś <patrykabras@gmail.com>
2019-04-08 18:58:17 +0200

    Poczatkowy wyglad + guzik goToHome

--- js/index.js
+++ js/index.js
@@ -1,17 +1,34 @@
-import 'ol/ol.css';
-import { Map, View } from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+let win;
 
-const map = new Map({
-    target: 'map',
-    layers: [
-        new TileLayer({
-            source: new OSM()
-        })
-    ],
-    view: new View({
-        center: [51, 15],
-        zoom: 5
-    })
-});
+function createWindow() {
+
+    win = new BrowserWindow({ width: 800, height: 600 })
+
+    //load index.html
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, '../index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    //open devtools
+    win.webContents.openDevTools();
+
+    //on close
+    win.on('close', () => {
+        win = null;
+    });
+}
+
+//run create window function
+app.on('ready', createWindow);
+
+//Quit when all window are closed
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+})
